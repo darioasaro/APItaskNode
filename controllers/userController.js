f64583e5a72bb292868cd3578c7fed6cc090c4a0
dario <darioasaro29@gmail.com>
2020-03-09 01:53:05 -0300

    login encrypted

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,33 +4,76 @@ var bcrypt = require('bcrypt');
 var BCRYPT_SALT_ROUNDS = 12;
 
 exports.login = (req, res) => {
-    var user = req.body.user;
-    var pass = req.body.pass;
     
-    console.log("user:", user, "pass:", pass);
+    console.log("body",req.body);
   
-    if (user && pass) {
-      db.connection.query(
-        `SELECT * FROM users WHERE name=? AND pass=?`,
-        [user, pass],
-        (err, rows) => {
-          if (err) {
-            res.status(500).json({ error: "dbError" });
-            throw err;
-          }
-         
-          if (rows.length > 0) {
-            res.json({ result: true, user: rows[0] });
-          } else {
-            res.status(404).json({ result: "unautorizhed" });
-          }
-        }
-      );
-    } else {
-      res.send("bad data");
-    }
-  };
+    //-----------------------
+    var username = req.body.user;
+    var password = req.body.pass;
+    if(username&&password){
 
+    db.connection.query(`SELECT pass from users WHERE name = ?`,[username],
+    (err,row)=>{
+        if(err){
+            res.status(500).json({'result':false,'message':'errorDb'})
+        }
+        else{
+            if(row.length>0){
+                bcrypt.compare(password, row[0].pass)
+                .then((samePass)=>{
+                    if(!samePass){
+                        res.status(403).json({'result':false,'message':'unAutorizhed'})
+                    }
+                    res.json({'result':true,'message':'ok'})
+
+                })
+
+            }
+            else{
+                res.status(403).json({'result':false,'message':'user not found'})
+            }
+        }
+    })
+   
+}   
+     
+
+
+    //-----------------------
+    
+    
+    
+    
+    
+    
+    
+    
+    // if (user && pass) {
+    //   db.connection.query(
+    //     `SELECT * FROM users WHERE name=? AND pass=?`,
+    //     [user, pass],
+    //     (err, rows) => {
+    //       if (err) {
+    //         res.status(500).json({ error: "dbError" });
+    //         throw err;
+    //       }
+         
+    //       if (rows.length > 0) {
+    //         res.json({ result: true, user: rows[0] });
+    //       } else {
+    //         res.status(404).json({ result: "unautorizhed" });
+    //       }
+    //     }
+    //   );
+    // } 
+    
+    else {
+      res.status(403).json({'result':false,'message':"bad data"});
+    }
+};
+
+
+  //----Crea el usuario en la base de datos con la contraseña encriptada
   exports.register = (req,res)=>{
      
     var username = req.body.user;
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,9 @@ const express = require( 'express' )
 const router = express.Router()
 
 
+router.post('/',(req,res)=>{
+    res.status(404).json({'result':false,'message':'recurso invalido'})
+})
 router.post('/:user',userController.login)
 router.post('/register/:user',userController.register)
 
